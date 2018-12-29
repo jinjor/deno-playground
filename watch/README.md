bb9f9d0ab55d3e6229488bc059c336b46ebc502b
jinjor <jinjorweb@gmail.com>
2018-12-29 17:16:29 +0900

    add benchmark and result

--- watch/README.md
+++ watch/README.md
@@ -1,5 +1,7 @@
 # Watch
 
+## Example
+
 ```typescript
 for await (const changes of watch(".")) {
   console.log(changes);
@@ -10,4 +12,24 @@ for await (const changes of watch(".")) {
 watch(tmpDir).start(changes => {
   console.log(changes);
 });
+```
+
+## Benchmark
+
+```
+test Benchmark
+generated 10930 files.
+[Add]
+took 183ms to traverse 11232 files
+took 147ms to traverse 11542 files
+took 142ms to traverse 11845 files
+[Modify]
+took 139ms to traverse 11891 files
+took 136ms to traverse 11891 files
+took 154ms to traverse 11891 files
+[Delete]
+took 138ms to traverse 11608 files
+took 134ms to traverse 11274 files
+took 145ms to traverse 10960 files
+... ok
 ```
--- watch/test.ts
+++ watch/test.ts
@@ -11,25 +11,28 @@ let tmpDir = env().TMPDIR || env().TEMP || env().TMP || "/tmp";
 test(async function Watch() {
   tmpDir = path.join(tmpDir, randomName("watch-test"));
   await mkdir(tmpDir);
-  let result = [];
-  const end = watch(tmpDir).start(changes => {
-    result = result.concat(changes);
-  });
   try {
-    const filePath = path.join(tmpDir, randomName("", ".txt"));
-    await writeFile(filePath, new Uint8Array(0));
-    await new Promise(resolve => setTimeout(resolve, 100));
-    assertEqual(result.length, 0);
-    await new Promise(resolve => setTimeout(resolve, 1200));
-    assertEqual(result.length, 1);
-    await writeFile(filePath, new Uint8Array(0));
-    await new Promise(resolve => setTimeout(resolve, 1200));
-    assertEqual(result.length, 2);
-    await remove(filePath);
-    await new Promise(resolve => setTimeout(resolve, 1200));
-    assertEqual(result.length, 3);
+    let result = [];
+    const end = watch(tmpDir).start(changes => {
+      result = result.concat(changes);
+    });
+    try {
+      const filePath = path.join(tmpDir, randomName("", ".txt"));
+      await writeFile(filePath, new Uint8Array(0));
+      await new Promise(resolve => setTimeout(resolve, 100));
+      assertEqual(result.length, 0);
+      await new Promise(resolve => setTimeout(resolve, 1200));
+      assertEqual(result.length, 1);
+      await writeFile(filePath, new Uint8Array(0));
+      await new Promise(resolve => setTimeout(resolve, 1200));
+      assertEqual(result.length, 2);
+      await remove(filePath);
+      await new Promise(resolve => setTimeout(resolve, 1200));
+      assertEqual(result.length, 3);
+    } finally {
+      end();
+    }
   } finally {
-    end();
     await removeAll(tmpDir);
   }
 });
@@ -37,13 +40,80 @@ test(async function Watch() {
 test(async function WatchByGenerator() {
   tmpDir = path.join(tmpDir, randomName("watch-test"));
   await mkdir(tmpDir);
-  const watcher = watch(tmpDir);
-  const filePath = path.join(tmpDir, randomName("", ".txt"));
-  setTimeout(async () => {
-    await writeFile(filePath, new Uint8Array(0));
-  }, 100);
-  for await (const changes of watcher) {
-    watcher.end();
+  try {
+    const watcher = watch(tmpDir);
+    const filePath = path.join(tmpDir, randomName("", ".txt"));
+    setTimeout(async () => {
+      await writeFile(filePath, new Uint8Array(0));
+    }, 100);
+    for await (const changes of watcher) {
+      watcher.end();
+    }
+  } finally {
+    await removeAll(tmpDir);
   }
-  await removeAll(tmpDir);
 });
+
+test(async function Benchmark() {
+  tmpDir = path.join(tmpDir, randomName("watch-test"));
+  await mkdir(tmpDir);
+  try {
+    const files = [];
+    await generateManyFiles(tmpDir, files);
+    console.log(`generated ${files.length} files.`);
+    const end = watch(tmpDir, {
+      log: s => {
+        console.log(s);
+      }
+    }).start(function() {});
+    try {
+      console.log("[Add]");
+      for (let i = 0; i < 1000; i++) {
+        await new Promise(resolve => setTimeout(resolve, 2));
+        let fileName = files[Math.floor(Math.random() * files.length)];
+        fileName = fileName + ".added";
+        await writeFile(fileName, new Uint8Array(0));
+      }
+      console.log("[Modify]");
+      for (let i = 0; i < 1000; i++) {
+        await new Promise(resolve => setTimeout(resolve, 2));
+        await writeFile(
+          files[Math.floor(Math.random() * files.length)],
+          new Uint8Array(0)
+        );
+      }
+      console.log("[Delete]");
+      for (let i = 0; i < 1000; i++) {
+        await new Promise(resolve => setTimeout(resolve, 2));
+        const index = Math.floor(Math.random() * files.length);
+        const fileName = files[index];
+        if (fileName) {
+          await remove(fileName);
+        }
+        files[index] = null;
+      }
+    } finally {
+      end();
+    }
+  } finally {
+    await removeAll(tmpDir);
+  }
+});
+const DEPTH = 7;
+const FILE_PER_DIR = 10;
+const DIR_PER_DIR = 3;
+async function generateManyFiles(dir, files, depth = DEPTH) {
+  if (depth <= 0) {
+    return;
+  }
+  for (let i = 0; i < FILE_PER_DIR; i++) {
+    const file = path.join(dir, randomName("", ".txt"));
+    files.push(file);
+    await writeFile(file, new Uint8Array(0));
+  }
+  for (let i = 0; i < DIR_PER_DIR; i++) {
+    const newDir = path.join(dir, randomName());
+    await mkdir(newDir);
+    await generateManyFiles(newDir, files, depth - 1);
+  }
+}
