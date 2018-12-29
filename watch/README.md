35a1721e8698c14ceddd048fd3e09acf595dc336
jinjor <jinjorweb@gmail.com>
2018-12-29 16:17:45 +0900

    update readme

--- watch/README.md
+++ watch/README.md
@@ -1,1 +1,13 @@
 # Watch
+
+```typescript
+for await (const changes of watch(".")) {
+  console.log(changes);
+}
+```
+
+```typescript
+watch(tmpDir).start(changes => {
+  console.log(changes);
+});
+```
--- watch/test.ts
+++ watch/test.ts
@@ -1,17 +1,13 @@
 import { writeFile, env, mkdir, remove } from "deno";
 import watch from "index.ts";
 import * as path from "https://deno.land/x/path/index.ts";
-import {
-  test,
-  assert,
-  assertEqual
-} from "https://deno.land/x/testing/testing.ts";
+import { test, assertEqual } from "https://deno.land/x/testing/testing.ts";
 
-function randomFileName() {
-  return Math.floor(Math.random() * 100000) + ".txt";
+function randomName(pre = "", post = ""): string {
+  return pre + Math.floor(Math.random() * 100000) + post;
 }
 let tmpDir = env().TMPDIR || env().TEMP || env().TMP || "/tmp";
-tmpDir = path.join(tmpDir, "watch-test");
+tmpDir = path.join(tmpDir, randomName("watch-test"));
 
 test(async function Watch() {
   await mkdir(tmpDir);
@@ -20,7 +16,7 @@ test(async function Watch() {
     result = result.concat(changes);
   });
   try {
-    const filePath = path.join(tmpDir, randomFileName());
+    const filePath = path.join(tmpDir, randomName("", ".txt"));
     await writeFile(filePath, new Uint8Array(0));
     await new Promise(resolve => setTimeout(resolve, 100));
     assertEqual(result.length, 0);
@@ -40,7 +36,7 @@ test(async function Watch() {
 test(async function WatchByGenerator() {
   await mkdir(tmpDir);
   const watcher = watch(tmpDir);
-  const filePath = path.join(tmpDir, randomFileName());
+  const filePath = path.join(tmpDir, randomName("", ".txt"));
   setTimeout(async () => {
     await writeFile(filePath, new Uint8Array(0));
   }, 100);
