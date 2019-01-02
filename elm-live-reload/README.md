6bf9635ca757186226447c17646c03f83b81793e
jinjor <jinjorweb@gmail.com>
2019-01-02 15:33:53 +0900

    switch back to raw.githubusercontent.com

--- elm-live-reload/README.md
+++ elm-live-reload/README.md
@@ -1,7 +1,7 @@
 # Elm Live Reload
 
 ```
-deno https://denopkg.com/jinjor/deno-playground/elm-live-reload/cli.ts src/Main.elm src/index.html --allow-net --allow-run
+deno https://raw.githubusercontent.com/jinjor/deno-playground/master/elm-live-reload/cli.ts src/Main.elm src/index.html --allow-net --allow-run
 ```
 
 ```
--- elm-live-reload/package.ts
+++ elm-live-reload/package.ts
@@ -3,6 +3,6 @@ import * as http from "https://deno.land/x/net/http.ts";
 import * as expressive from "../expressive/index.ts";
 import watch from "../watch/index.ts";
 import * as path from "https://deno.land/x/path/index.ts";
-import { opn } from "https://denopkg.com/hashrock/deno-opn/opn.ts";
+import { opn } from "https://raw.githubusercontent.com/hashrock/deno-opn/master/opn.ts";
 
 export { flags, http, expressive, path, opn, watch };
