792ba0f5c802c176c140cda945dcdb0e8f444161
jinjor <jinjorweb@gmail.com>
2019-01-05 06:59:07 +0900

    add doc

--- task-runner/counter.ts
+++ task-runner/counter.ts
@@ -1,7 +1,7 @@
 import { args } from "deno";
 let i = 0;
 const interval = setInterval(() => {
-  console.log(args[1], ++i);
+  console.log(args[1] || "", ++i);
   if (i >= 5) {
     clearInterval(interval);
   }
--- task-runner/example.ts
+++ task-runner/example.ts
@@ -3,4 +3,4 @@ import { task } from "mod.ts";
 task("prepare", "echo preparing...");
 task("counter", "deno counter.ts");
 task("thumb", "deno https://deno.land/thumb.ts");
-task("test", "prepare", ["counter alice", "counter bob"], "thumb");
+task("all", "prepare", ["counter alice", "counter bob"], "thumb");
